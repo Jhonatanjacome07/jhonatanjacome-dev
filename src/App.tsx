475792293bdf2624b69ca60b19433334291d1d42
Jhonatan Jacome <jhonatanjacome07@gmail.com>
2026-01-03 19:22:46 -0500

    fix: remove unused React import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import ServiceSummary from "./sections/ServiceSummary";
